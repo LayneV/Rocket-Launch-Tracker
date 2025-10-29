1b6c0ce01821cf41b27b7daa0983ce12e43fd688
LayneV <152360779+LayneV@users.noreply.github.com>
2025-10-28 19:49:44 -0500

    feat: Create logic to display launches

--- src/components/LaunchList.jsx
+++ src/components/LaunchList.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 
 function LaunchList() {
-    return (
-        <div>
-            <h2>Upcoming Launches</h2>
-        </div>
-    )
+  const [launches, setLaunches] = useState(["Launch 1"]);
+  return (
+    <div>
+      <h2>Upcoming Launches</h2>
+      <ul>
+        {launches.map((launch, index) => (
+          <li key={index}>{launch}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default LaunchList
+export default LaunchList;
