aa45ad462b00068530f7399eb0e4e4afe225e66b
LayneV <152360779+LayneV@users.noreply.github.com>
2025-10-30 15:26:55 -0500

    feat: Add upcoming/previous launches to homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,8 +1,78 @@
+import { useEffect, useState } from "react";
+import LaunchCard from "./LaunchCard";
+import { Link } from "react-router-dom";
+import { Button } from "flowbite-react";
 
 function HomePage() {
-    return (
-        <h1>Welcome to Rocket Launch Tracker!</h1>
-    )
+  const [upcoming, setUpcoming] = useState([]);
+  const [previous, setPrevious] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchHomepageData = async () => {
+      const [upcomingResponse, previousResponse] = await Promise.all([
+        fetch(
+          "https://lldev.thespacedevs.com/2.3.0/launches/upcoming/?limit=3"
+        ),
+        fetch(
+          "https://lldev.thespacedevs.com/2.3.0/launches/previous/?limit=3"
+        ),
+      ]);
+
+      const previousData = await previousResponse.json();
+      const upcomingData = await upcomingResponse.json();
+
+      setPrevious(previousData.results);
+      setUpcoming(upcomingData.results);
+      setLoading(false);
+    };
+    fetchHomepageData();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <div className="container mx-auto p-4 flex flex-col gap-8">
+      <h1 className="text-4xl font-bold text-center dark:text-white">
+        Welcome to Rocket Launch Tracker!
+      </h1>
+      <section className="dark:bg-slate-800 p-5 rounded-lg border border-slate-700">
+        <div className="flex justify-between items-center mb-4">
+          <h2 className="text-2xl font-semibold dark:text-white">
+            Upcoming Launches
+          </h2>
+          <Link to="/upcoming">
+            <Button>View All</Button>
+          </Link>
+        </div>
+        <div className="flex flex-row gap-4">
+          {upcoming.map((launch) => (
+            <div key={launch.id}>
+              <LaunchCard launch={launch} />
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className="dark:bg-slate-800 p-5 rounded-lg border border-slate-700">
+        <div className="flex justify-between items-center">
+          <h2 className="text-2xl font-semibold dark:text-white">
+            Previous Launches
+          </h2>
+          <Link to="/previous">
+            <Button>View All</Button>
+          </Link>
+        </div>
+
+        <div className="flex flex-row gap-4">
+          {previous.map((launch) => (
+            <div key={launch.id}>
+              <LaunchCard launch={launch} />
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
