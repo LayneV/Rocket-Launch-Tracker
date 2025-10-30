5e3ca7b210d9b9c3299212879c166abbf250ea46
LayneV <152360779+LayneV@users.noreply.github.com>
2025-10-30 12:58:27 -0500

    feat: Create Homepage component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import LaunchList from "./components/LaunchList";
 import { Routes, Route, Link } from "react-router-dom";
 import LaunchDetail from "./components/LaunchDetail";
 import Header from "./components/Header";
+import HomePage from "./components/Homepage";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
       <Header />
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/upcoming" element={<LaunchList listType="upcoming" />} />
         <Route path="/launch/:id" element={<LaunchDetail />} />
         <Route path="/previous" element={<LaunchList listType="previous" />} />
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,8 @@
+
+function HomePage() {
+    return (
+        <h1>Welcome to Rocket Launch Tracker!</h1>
+    )
+}
+
+export default HomePage
