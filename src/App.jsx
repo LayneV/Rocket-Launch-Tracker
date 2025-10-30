5159b2efdecdac4efb0cb5141fb16a721ffa9715
LayneV <152360779+LayneV@users.noreply.github.com>
2025-10-29 20:58:04 -0500

    feat: Create Header component that is always visible

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { Component } from "react";
 import LaunchList from "./components/LaunchList";
 import { Routes, Route, Link } from "react-router-dom";
 import LaunchDetail from "./components/LaunchDetail";
+import Header from "./components/Header";
 
 function App() {
   return (
+    <div>
+    <Header/>
     <Routes>
       <Route path="/" element={<LaunchList />} />
       <Route path="/launch/:id" element={<LaunchDetail />} />
     </Routes>
+    </div>
   );
 }
 
